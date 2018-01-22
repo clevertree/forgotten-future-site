4bceab5b6f79ad6459f525fd22def6a8aa2ec5f1
Ari Asulin <ari.asulin@gmail.com>
2018-01-21 19:04:07 -0700

    test files

---
+++ ffsite/test.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ari
+ * Date: 1/19/2018
+ * Time: 2:43 PM
+ */
+
+set_include_path(dirname(__DIR__, 1));
+spl_autoload_register();
+
+\FFSite\Table\UserRow::_test();
