ca3dbf6e5e37e051be156bfb88f53583a3b3cf62
Ari Asulin <ari.asulin@gmail.com>
2018-01-24 17:55:11 -0700

    fixes

--- ffsite/git.php
+++ ffsite/git.php
@@ -17,6 +17,7 @@ if($params && $params[0] === '{') {
 
 echo "Executing Git Pull...\n";
 echo exec("git pull");
+echo exec("git submodule update --recursive --remote");
 
 echo "\nParameters: \n";
 print_r ($params);
