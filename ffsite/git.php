1556688ebd95ae3b0a3bda34832f9186148b4c3a
Ari Asulin <ari.asulin@gmail.com>
2018-06-01 01:26:14 +0000

    readded git

--- ffsite/git.php
+++ ffsite/git.php
@@ -8,6 +8,10 @@
 
 header("Content-Type: text/plain");
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $params = @$_REQUEST['payload'] ?: file_get_contents('php://input');
 if($params && $params[0] === '{') {
     $params = json_decode($params, true);
