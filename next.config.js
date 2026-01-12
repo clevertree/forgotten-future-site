9ee50fef2c99aaa52755192311407a74535cadb1
Ari Asulin <ari.asulin@gmail.com>
2026-01-12 06:04:16 -0500

    Remove unused MDX support and dependencies

--- next.config.js
+++ next.config.js
@@ -1,24 +1,12 @@
 // next.config.js
 
 const path = require("path");
-const withMDX = require('@next/mdx')({
-    extension: /\.mdx?$/,
-    options: {
-        // If you use remark-gfm, you'll need to use next.config.mjs
-        // as the package is ESM only
-        // https://github.com/remarkjs/remark-gfm#install
-        remarkPlugins: [],
-        rehypePlugins: [],
-        // If you use `MDXProvider`, uncomment the following line.
-        // providerImportSource: "@mdx-js/react",
-    },
-})
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: process.env.NEXT_PUBLIC_IS_STATIC === 'true' ? 'export' : undefined,
-    // Configure pageExtensions to include md and mdx
-    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+    // Configure pageExtensions
+    pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
     // Optionally, add any other Next.js config below
     reactStrictMode: true,
     sassOptions: {
@@ -33,12 +21,4 @@ const nextConfig = {
     }
 }
 
-// Merge MDX config with Next.js config
-try {
-    module.exports = withMDX(nextConfig)
-} catch (error) {
-    if (!(error instanceof Error)) {
-        throw new Error(String(error))
-    }
-    throw error
-}
+module.exports = nextConfig
